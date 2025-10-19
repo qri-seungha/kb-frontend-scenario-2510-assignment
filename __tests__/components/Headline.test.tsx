355a81f88f7c08c98fd066d571e2355193514c49
qri-seungha <115711453+qri-seungha@users.noreply.github.com>
2025-10-19 21:29:12 +0900

    test Headline

--- __tests__/components/Headline.test.tsx
+++ __tests__/components/Headline.test.tsx
@@ -9,9 +9,16 @@ jest.mock('@/styles/fonts', () => ({
 describe('<Headline>', () => {
   test('Headline 컴포넌트가 정상적으로 렌더링된다', () => {
     render(<Headline />)
+    const headline = screen.getByTestId('headline')
+    expect(headline).toBeInTheDocument()
   })
 
   test('메인 제목이 정상적으로 렌더링된다', () => {
     render(<Headline />)
+    const heading = screen.getByRole('heading', { level: 1 })
+    expect(heading).toBeInTheDocument()
+    expect(screen.getByText('OMDb API')).toBeInTheDocument()
+    expect(screen.getByText(/THE OPEN/i)).toBeInTheDocument()
+    expect(screen.getByText(/MOVIE DATABASE/i)).toBeInTheDocument()
   })
 })
